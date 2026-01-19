43de40cd9499bc5f2a272b88d6dec5c0b3922f22
fita8547 <junsumon090608@dgsw.hs.kr>
2026-01-19 12:47:56 +0900

    add:splash.dartFeat:home.dart

---
+++ lib/screens/home.dart
@@ -0,0 +1,8 @@
+import 'dart:convert'; //json해석
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //assets 불러오기용
+import 'detail.dart';  //상세 페이지
+
+class HomePage extends StatefulWidget{
+
+}
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -15,7 +15,25 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
+    Future.delayed(const Duration(seconds: 2),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder:(_)=>HomePage()),
+      );
+      //현재화면 지우고 홈 페이지 화면 뛰우
+    })
   }
-
-
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "나의  건강 데이터",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
+
+//* initState() → 시작하자마자 실행
+// * Future.delayed → 스플래시 2초 유지
+// * Navigator.pushReplacement → 다음 화면 이동
